982f5a100a2174df687d74627e826d3adc2f4681
Juan <helloteam.developer2@gmail.com>
2022-05-16 15:44:30 -0500

    area conocenos

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+< lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -59,6 +59,33 @@
             @yield('content')
         </div>
     </main>
+
+    <section style="background-color: #FFFF">
+        <div class="container pb-5 text-center">
+            <br>
+            <h2 class="titulo">¿Por qué elegirnos?</h2>
+            <div class="row text-center">
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 12.png') }}">
+                    <p class="eligenos">Tu préstamo inmediato sin salir de casa</p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 11.png') }}">
+                    <p class="eligenos">Préstamo confiable</p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 10.png') }}">
+                    <p class="eligenos">Plazo de 1 a 6 meses </p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 9.png') }}">
+                    <p class="eligenos">Opciones de pago</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <x-package-alert titulo="terminos-condiciones" idm="terminosCondiciones"/>
     <x-package-alert titulo="quienes-somos" idm="quienesSomos"/>
     <x-package-alert titulo="acerca-nosotros" idm="acercaNosotros"/>
