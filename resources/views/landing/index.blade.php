5fd48be114faef0b853272418e902c6cb7272949
CRIZPRZ <al221711754@gmail.com>
2022-05-17 17:53:06 -0500

    aasd

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -2,10 +2,43 @@
 @section('content')
 
 
-    <section class="row">
+    <section class="row g-0 px-5">
+        <div class="">
 
-        <div class="col-12">
-            <img src="{{ asset('img/landing/index/Grupo 246.png') }}" width="100%" height="600">
+        </div>
+        <div class="col-md-6 div-calcular">
+            <div class="div-calcular">
+                <div class="container" style="padding: 0 150px 0 150px">
+
+                    <div class="mt-5 text-center mb-5">
+                        <h1>¿Cu&aacute;nto <span class="text-green">dinero</span> necesitas?</h1>
+                    </div>
+
+                    <div class="text-left mt-5">
+                        <h3><strong>Pr&eacute;stamo</strong></h3>
+                    </div>
+
+                    <div>
+                        <livewire:landing.slider-prestamo/>
+                    </div>
+
+                    <div class="text-left mt-5">
+                        <h3><strong>Tiempo de pr&eacute;stamo</strong></h3>
+                    </div>
+
+                    <div class="mb-5">
+                        <livewire:landing.slider-tiempo/>
+                    </div>
+
+                    <div class="col-12 mt-5 mb-5 text-center">
+                        <button class="btn btn-verde"><strong>Calcular</strong></button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="{{ asset('img/landing/index/Content-Figure.png') }}" width="100%"">
         </div>
 
         <div class="col-12 bg-gray mb-5">
@@ -36,37 +69,34 @@
                 </div>
             </div>
         </div>
-        <div class="div-calcular">
-            <div class="container" style="padding: 0 150px 0 150px">
 
-                <div class="mt-5 text-center mb-5">
-                    <h1>¿Cu&aacute;nto <span class="text-green">dinero</span> necesitas?</h1>
-                </div>
-
-                <div class="text-left mt-5">
-                    <h3><strong>Pr&eacute;stamo</strong></h3>
-                </div>
-
-                <div>
-                    <livewire:landing.slider-prestamo/>
-                </div>
-
-                <div class="text-left mt-5">
-                    <h3><strong>Tiempo de pr&eacute;stamo</strong></h3>
-                </div>
-
-                <div class="mb-5">
-                    <livewire:landing.slider-tiempo/>
-                </div>
-
-                <div class="col-12 mt-5 mb-5 text-center">
-                    <button class="btn btn-verde"><strong>Calcular</strong></button>
-                </div>
-            </div>
-
-        </div>
     </section>
 
+
+    <section style="background-color: #FFFF">
+        <div class="container pb-5 text-center">
+            <br>
+            <h2 class="titulo">¿Por qué elegirnos?</h2>
+            <div class="row text-center">
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 12.png') }}">
+                    <p class="eligenos">Tu préstamo inmediato sin salir de casa</p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 11.png') }}">
+                    <p class="eligenos">Préstamo confiable</p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 10.png') }}">
+                    <p class="eligenos">Plazo de 1 a 6 meses </p>
+                </div>
+                <div class="col-xs-12 col-md-3 mt-5">
+                    <img src="{{ asset('img/landing/index/Enmascarar grupo 9.png') }}">
+                    <p class="eligenos">Opciones de pago</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 <script>
--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -65,33 +65,6 @@
     <x-modal-footer titulo="acerca-nosotros" idm="acercaNosotros"/>
     <x-modal-footer titulo="politica-privacidad" idm="politicaPrivacidad"/>
 
-
-    <section style="background-color: #FFFF">
-        <div class="container pb-5 text-center">
-            <br>
-            <h2 class="titulo">¿Por qué elegirnos?</h2>
-            <div class="row text-center">
-                <div class="col-xs-12 col-md-3 mt-5">
-                    <img src="{{ asset('img/landing/index/Enmascarar grupo 12.png') }}">
-                    <p class="eligenos">Tu préstamo inmediato sin salir de casa</p>
-                </div>
-                <div class="col-xs-12 col-md-3 mt-5">
-                    <img src="{{ asset('img/landing/index/Enmascarar grupo 11.png') }}">
-                    <p class="eligenos">Préstamo confiable</p>
-                </div>
-                <div class="col-xs-12 col-md-3 mt-5">
-                    <img src="{{ asset('img/landing/index/Enmascarar grupo 10.png') }}">
-                    <p class="eligenos">Plazo de 1 a 6 meses </p>
-                </div>
-                <div class="col-xs-12 col-md-3 mt-5">
-                    <img src="{{ asset('img/landing/index/Enmascarar grupo 9.png') }}">
-                    <p class="eligenos">Opciones de pago</p>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
     <footer>
         <div class="container pb-5">
             <div class="row">
