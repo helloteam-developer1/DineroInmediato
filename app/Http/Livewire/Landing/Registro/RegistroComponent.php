cd811b79642bbbeba269d2b1ab25f04a7a3f2d9b
crizprz <al221711754@gmail.com>
2022-05-22 12:35:11 -0500

    'step'

--- app/Http/Livewire/Landing/Registro/RegistroComponent.php
+++ app/Http/Livewire/Landing/Registro/RegistroComponent.php
@@ -7,7 +7,15 @@ use Livewire\Component;
 class RegistroComponent extends Component
 {
     public $step = 1;
-    public $name;
+    public $form =[
+        'name' => null,
+        'curp' => null,
+        'fecha_nacimiento' => null,
+        'empresa_trabaja' => null,
+        'antiguedad' => null,
+        'rama_empresa' => null,
+        'banco_nomina' => null,
+    ];
 
     public function render()
     {
@@ -17,7 +25,13 @@ class RegistroComponent extends Component
     public function stepOne()
     {
         $this->validate([
-            'name' => 'required'
+            'form.name' => 'required',
+            'form.curp' => 'required',
+            'form.fecha_nacimiento' => 'required',
+            'form.empresa_trabaja' => 'required',
+            'form.antiguedad' => 'required',
+            'form.rama_empresa' => 'required',
+            'form.banco_nomina' => 'required',
         ]);
 
         $this->step = 2;
