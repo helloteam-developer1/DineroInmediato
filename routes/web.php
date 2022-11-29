72e17a15829c6aecd2d234fbf1f61663318aebf6
helloteamdeveloper2 <hello.developer2@gmail.com>
2022-11-29 14:28:45 -0600

    se añadio rutas de backoffices

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,5 @@ use Illuminate\Support\Facades\Route;
 include('landing/web.php');
 
 include('dashboard/web.php');
-
-<<<<<<< HEAD
 include('backoffices/web.php');
-=======
->>>>>>> 24a37b4712fd6a5228c3315ee62def1c155755e9
+
