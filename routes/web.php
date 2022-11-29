15cfe0382307add3f5e7fb0897b28e697a12dea2
helloteamdeveloper2 <hello.developer2@gmail.com>
2022-11-29 10:54:14 -0600

    se añadieron las rutas para la carga de las vistas de backofficesweb

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\backoffices\clientesController;
 use App\Http\Controllers\landing\registroController;
 use Illuminate\Support\Facades\Route;
 
 include('landing/web.php');
 
 include('dashboard/web.php');
+
+include('backoffices/web.php');
