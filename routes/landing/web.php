120835afd47911300b3871fbadddddf4928a3b7c
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2023-03-01 22:39:32 -0600

    produccion

--- routes/landing/web.php
+++ routes/landing/web.php
@@ -38,7 +38,8 @@ Route::resource('/registros-calculadora', CalculadoraRegistro::class);
 Route::middleware('guest')->group(function () {
     Route::GET('/',function(){
         if ( env('APP_ENV') === 'local' ) {
-            return view('landing.index');
+            //return view('landing.index');
+            return view('construccion');
         }else {
             return view('construccion');
         }
