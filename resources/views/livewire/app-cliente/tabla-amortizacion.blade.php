61e1ca555800c667c7c5077b6fce147d428e2a1a
Luis Aguilera <luisx9414@gmail.com>
2023-08-04 07:52:52 -0600

    [Update] TabAmortizaccion Cambio Estilos tabla

--- resources/views/livewire/app-cliente/tabla-amortizacion.blade.php
+++ resources/views/livewire/app-cliente/tabla-amortizacion.blade.php
@@ -1,35 +1,73 @@
+<style>
+    /* Estilos para las cajas de entrada de fecha en pantallas grandes */
+    /* Estilos para las cajas de entrada de fecha en pantallas grandes */
+
+    /* Estilos para las cajas de entrada de fecha en dispositivos móviles */
+    @media (max-width: 767px) {
+        input[type="date"] {
+            text-align: center;
+            height: 40px; /* Altura deseada para la caja de entrada en móviles */
+            font-size: 10px; /* Tamaño de fuente deseado para la caja de entrada en móviles */
+            padding: 4px; /* Espacio interno deseado para la caja de entrada en móviles */
+            min-width: 80px; /* Ancho deseado para la caja de entrada en móviles */
+            max-width: 80px; /* Ancho deseado para la caja de entrada en móviles */
+        }
+    }
+
+    @media (max-width: 767px) {
+    .imagen-estilo {
+        width: 80%; /* Ancho deseado para la imagen en dispositivos móviles */
+        height: auto; /* Para mantener la proporción de la imagen */
+    }
+    @media (max-width: 767px) {
+        .btn-lg {
+            width: 100%;
+        }
+    }
+}
+
+
+
+</style>
+
 <div>
     <div class="container">
         <br />
-        <h1 class="text-3xl font-bold text-datgencred text-center" style="color: #F5A733;">
+        <h1 class="text-3xl font-bold text-datgencred text-center" style="color: #F27C00; font-size: 1.6rem;"">
             Tabla de amortización
         </h1>
         
         <br>
-        <div class="d-flex justify-content-center">
-            <div class="row g-3 ">
-                <div class="col-auto">
+
+        <div class="d-flex flex-column flex-lg-row justify-content-center">
+            <div class="row g-3">
+                <div class="col-auto ">
                     <label for="inputPassword6" class="col-form-label fw-bold">De</label>
                 </div>
                 <div class="col-auto">
-                    <input type="date" id="inputPassword6" class="form-control"
-                        aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_inicio">
+                    <input type="date" id="inputPassword6" class="form-control" style="width: 123px;"
+                    aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_inicio">
+
                 </div>
-                <div class="col-auto">
-                    <img src="img/Grupo 166.png" alt="">
+
+                <div class="col-auto ">      
+                    <img src="../img/CALENDARIO.svg" class="imagen-estilo" alt="">
                 </div>
-                <div class="col-auto">
+                <div class="col-auto px-0 py-0">
                     <label for="inputPassword6" class="col-form-label fw-bold">a</label>
                 </div>
-                <div class="col-auto">
-                    <input type="date" id="inputPassword6" class="form-control"
+                
+                <div class="col-auto ">
+                    <input type="date" id="inputPassword6" class="form-control" style="width: 123px;"
                         aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_termino">
                 </div>
-                <div class="col-auto">
-                    <img src="img/Grupo 166.png" alt="">
+                <div class="col-auto px-0 py-0">
+                    <img src="../img/CALENDARIO.svg" class="imagen-estilo" alt="">
                 </div>
-                <div class="col-auto">
-                    <div class="input-group w-60">
+
+                
+                <div class="col-12 col-md-auto">
+                    <div class="input-group">
                         <span class="input-group-text" id="basic-addon1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                 fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
@@ -42,12 +80,21 @@
                             aria-describedby="basic-addon1" wire:model="busqueda">
                     </div>
                 </div>
-                <div class="col-auto">
-                    <button type="button" class="btn btn-success" style="background: #4A9D22;">Buscar</button>
+
+                <div class="col-12 col-md-auto">
+                    <div class="col-12 d-flex justify-content-center">
+                        <button type="button" class="btn btn-success btn-lg d-flex align-items-center justify-content-center w-100" style="background: #4A9D22; height: 40px; padding: 20;">
+                            Buscar
+                        </button>
+                    </div>
                 </div>
+
+
+
             </div>
         </div>
-        <br />
+        
+        <br>
         <div class="table-responsive">
             <table class="table table-striped">
                 <thead>
@@ -56,56 +103,52 @@
                         <th class="border text-center" scope="col"># de pagos</th>
                         <th class="border text-center" scope="col">Próximo pago</th>
                         <th class="border text-center" scope="col">Interés Anual (CAT informativo sin IVA)</th>
-                        <th class="border text-center" scope="col">Pago a Capital</th> 
-                        <th class="border text-center" scope="col">Intereses Ordinarios</th>
-                        <th class="border text-center" scope="col">IVA interes Ordinarios</th>
-                        <th class="border text-center" scope="col">Comisiones</th>
-                        <th class="border text-center" scope="col">Pago total mensual</th>                         
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Pago a Capital</th> 
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Intereses Ordinarios</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">IVA interes Ordinarios</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Comisiones</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Pago total mensual</th>                         
                     </tr>
                 </thead>
                 <tbody>
-                        @if ($tabla!=null)
-                            @if ($tabla->count())
-                                @foreach ($tabla as $t)
-                                
-                                    @php
-                                        $total_interes_ordinario += $t->interes_ordinarios;
-                                        $total_iva_io+= $t->iva_io;
-                                        $total_comisiones+= $t->comisiones;
-                                        $total_pago_m+= $t->pago_total_men;                     
-                                    @endphp
-                                    <tr>
-                                        <th class="border text-center" scope="col">{{$t->num_credito}}</th>
-                                        <th class="border text-center" scope="col">{{$t->numero_pagos}}</th>
-                                        <th class="border text-center" scope="col" style="white-space: nowrap;">{{$t->prox_pago}}</th>
-                                        <th class="border text-center" scope="col">{{$t->interes_anual}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($t->pag_capital)}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($t->interes_ordinarios)}}</th>
-                                        <th class="border text-center" scope="col">{{$t->iva_io}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($t->comisiones)}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($t->pago_total_men)}}</th>
-                                    </tr>
-                                    
-                                
-                                @endforeach
+                    @if ($tabla!=null)
+                        @if ($tabla->count())
+                            @foreach ($tabla as $t)
+                                @php
+                                    $total_interes_ordinario += $t->interes_ordinarios;
+                                    $total_iva_io+= $t->iva_io;
+                                    $total_comisiones+= $t->comisiones;
+                                    $total_pago_m+= $t->pago_total_men;                     
+                                @endphp
                                 <tr>
-                                        <th class="border text-center" scope="col" colspan="5">Total</th>
-                                        <th class="border text-center" scope="col">{{number_format($total_interes_ordinario)}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($total_iva_io)}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($total_comisiones)}}</th>
-                                        <th class="border text-center" scope="col">{{number_format($total_pago_m)}}</th>
+                                    <th class="border text-center" scope="col">{{$t->num_credito}}</th>
+                                    <th class="border text-center" scope="col">{{$t->numero_pagos}}</th>
+                                    <th class="border text-center" scope="col" style="white-space: nowrap;">{{$t->prox_pago}}</th>
+                                    <th class="border text-center" scope="col">{{$t->interes_anual}}</th>
+                                    <th class="border text-center d-none d-md-table-cell" scope="col">{{number_format($t->pag_capital)}}</th>
+                                    <th class="border text-center d-none d-md-table-cell" scope="col">{{number_format($t->interes_ordinarios)}}</th>
+                                    <th class="border text-center d-none d-md-table-cell" scope="col">{{$t->iva_io}}</th>
+                                    <th class="border text-center d-none d-md-table-cell" scope="col">{{number_format($t->comisiones)}}</th>
+                                    <th class="border text-center d-none d-md-table-cell" scope="col">{{number_format($t->pago_total_men)}}</th>
                                 </tr>
-                            @else
+                            @endforeach
+                            <tr>
+                                <th class="border text-center" scope="col" colspan="5">Total</th>
+                                <th class="border text-center" scope="col">{{number_format($total_interes_ordinario)}}</th>
+                                <th class="border text-center" scope="col">{{number_format($total_iva_io)}}</th>
+                                <th class="border text-center" scope="col">{{number_format($total_comisiones)}}</th>
+                                <th class="border text-center" scope="col">{{number_format($total_pago_m)}}</th>
+                            </tr>
+                        @else
                             <tr>
                                 <th class="border text-center" colspan="9">Sin registros.</th>
                             </tr>
-                            @endif
-                        @else
-                            <tr>
-                                <th class="border text-center" scope="col" colspan="9">Sin registros.</th>
-                                
-                            </tr>
                         @endif
+                    @else
+                        <tr>
+                            <th class="border text-center" scope="col" colspan="9">Sin registros.</th>
+                        </tr>
+                    @endif
                 </tbody>
             </table>
         </div>
