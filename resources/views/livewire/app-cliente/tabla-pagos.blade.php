2bc93e21d12fa724d69d06174f4f4d97cc46d2ed
Luis Aguilera <luisx9414@gmail.com>
2023-08-04 07:51:50 -0600

    [Update] Tabla Pagos Cambios en estilos tabla

--- resources/views/livewire/app-cliente/tabla-pagos.blade.php
+++ resources/views/livewire/app-cliente/tabla-pagos.blade.php
@@ -1,35 +1,70 @@
+<style>
+
+    /* Estilos para las cajas de entrada de fecha en dispositivos móviles */
+    @media (max-width: 767px) {
+        input[type="date"] {
+            text-align: center;
+            height: 40px; /* Altura deseada para la caja de entrada en móviles */
+            font-size: 10px; /* Tamaño de fuente deseado para la caja de entrada en móviles */
+            padding: 4px; /* Espacio interno deseado para la caja de entrada en móviles */
+            min-width: 80px; /* Ancho deseado para la caja de entrada en móviles */
+            max-width: 80px; /* Ancho deseado para la caja de entrada en móviles */
+        }
+    }
+
+    @media (max-width: 767px) {
+    .imagen-estilo {
+        width: 80%; /* Ancho deseado para la imagen en dispositivos móviles */
+        height: auto; /* Para mantener la proporción de la imagen */
+    }
+    @media (max-width: 767px) {
+        .btn-lg {
+            width: 100%;
+        }
+    }
+}
+
+
+
+</style>
+
 <div>
-    
     <div class="container">
         <br />
-        <h1 class="text-3xl font-bold text-datgencred text-center" style="color: #F5A733;">
+        <h1 class="text-3xl font-bold text-datgencred text-center" style="color: #F27C00; font-size: 1.6rem;">
             Tabla de pagos
         </h1>
         <br>
-        <div class="d-flex justify-content-center">
+        
+        <div class="d-flex flex-column flex-lg-row justify-content-center">
             <div class="row g-3">
-                <div class="col-auto">
+                <div class="col-auto ">
                     <label for="inputPassword6" class="col-form-label fw-bold">De</label>
                 </div>
                 <div class="col-auto">
-                    <input type="date" id="inputPassword6" class="form-control"
-                        aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_inicio">
+                    <input type="date" id="inputPassword6" class="form-control" style="width: 123px;"
+                    aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_inicio">
+
                 </div>
-                <div class="col-auto">      
-                    <img src="img/Grupo 166.png" alt="">
+
+                <div class="col-auto ">      
+                    <img src="../img/CALENDARIO.svg" class="imagen-estilo" alt="">
                 </div>
-                <div class="col-auto">
+                <div class="col-auto px-0 py-0">
                     <label for="inputPassword6" class="col-form-label fw-bold">a</label>
                 </div>
-                <div class="col-auto">
-                    <input type="date" id="inputPassword6" class="form-control"
+                
+                <div class="col-auto ">
+                    <input type="date" id="inputPassword6" class="form-control" style="width: 123px;"
                         aria-describedby="passwordHelpInline" wire:model.debounce.900ms="fecha_termino">
                 </div>
-                <div class="col-auto">
-                    <img src="img/Grupo 166.png" alt="">
+                <div class="col-auto px-0 py-0">
+                    <img src="../img/CALENDARIO.svg" class="imagen-estilo" alt="">
                 </div>
-                <div class="col-auto">
-                    <div class="input-group w-60">
+
+                
+                <div class="col-12 col-md-auto">
+                    <div class="input-group">
                         <span class="input-group-text" id="basic-addon1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                 fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
@@ -42,66 +77,71 @@
                             aria-describedby="basic-addon1" wire:model="busqueda">
                     </div>
                 </div>
-                <div class="col-auto">
-                    <button type="button" class="btn btn-success" style="background: #4A9D22;">Buscar</button>
+
+                <div class="col-12 col-md-auto">
+                    <div class="col-12 d-flex justify-content-center">
+                        <button type="button" class="btn btn-success btn-lg d-flex align-items-center justify-content-center w-100" style="background: #4A9D22; height: 40px; padding: 20;">
+                            Buscar
+                        </button>
+                    </div>
                 </div>
+
+
+
             </div>
         </div>
-        <br />
-        <table class="table table-striped" id="tablapagos">
-            <thead>
-                <tr>
-                    <th class="border text-center" scope="col">Pago número</th>
-                    <th class="border text-center" scope="col">Fecha de pago</th>
-                    <th class="border text-center" scope="col">Monto de pago</th>
-                    <th class="border text-center" scope="col">Saldo insoluto</th>
-                    <th class="border text-center" scope="col"># pagos restantes</th>
-                    <th class="border text-center" scope="col">Resta por pagar</th>
-                    <th class="border text-center" scope="col">Numero de credito</th>
-                    <!-- <td class="" style="border: none;">
-                </td> -->
-            </tr>
-            </thead>
-            
-            @if ($pagos==null)
-                <tbody>
+        
+        <br>
+
+        <div class="table-responsive">
+            <table class="table table-striped" id="tablapagos">
+                <thead>
                     <tr>
-                        <th colspan="7" class="border text-center">Sin registros.</th>
+                        <th class="border text-center" scope="col">Pago número</th>
+                        <th class="border text-center" scope="col">Fecha de pago</th>
+                        <th class="border text-center" scope="col">Monto de pago</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Saldo insoluto</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col"># pagos restantes</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Resta por pagar</th>
+                        <th class="border text-center d-none d-md-table-cell" scope="col">Numero de credito</th>
                     </tr>
-                </tbody>
-            @else
-                <tbody>
-                    @if ($pagos->count())
-                        @foreach ($pagos as $p)
+                </thead>
+                @if ($pagos == null)
+                    <tbody>
+                        <tr>
+                            <td colspan="7" class="border text-center">Sin registros.</td>
+                        </tr>
+                    </tbody>
+                @else
+                    <tbody>
+                        @if ($pagos->count())
+                            @foreach ($pagos as $p)
                                 <tr>
-                                    <th class="border text-center">{{$p->numero_pagos}}</th>
-                                    <th class="border text-center">{{$p->fecha_pago}}</th>
-                                    <th class="border text-center">{{$p->monto_pago}}</th>
-                                    <th class="border text-center">{{$p->saldo_insoluto}}</th>
-                                    <th class="border text-center">{{$p->pago_rest}}</th>
-                                    <th class="border text-center">{{$p->resta_pagar}}</th>
-                                    <th class="border text-center">{{$p->num_credito}}</th>
+                                    <td class="border text-center">{{$p->numero_pagos}}</td>
+                                    <td class="border text-center">{{$p->fecha_pago}}</td>
+                                    <td class="border text-center">{{$p->monto_pago}}</td>
+                                    <td class="border text-center d-none d-md-table-cell">{{$p->saldo_insoluto}}</td>
+                                    <td class="border text-center d-none d-md-table-cell">{{$p->pago_rest}}</td>
+                                    <td class="border text-center d-none d-md-table-cell">{{$p->resta_pagar}}</td>
+                                    <td class="border text-center d-none d-md-table-cell">{{$p->num_credito}}</td>
                                 </tr>
-                        @endforeach
-                    @else
-                    <tr>
-                        <th class="border text-center" colspan="7">Sin registros.</th>
-                    </tr>
-                    @endif
-                    
-                </tbody>
-            @endif
-        </table>
-
-        <div class="row">
-
+                            @endforeach
+                        @else
+                            <tr>
+                                <td class="border text-center" colspan="7">Sin registros.</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                @endif
+            </table>
         </div>
-    </div>
 
+        
+    </div>
     <div class="container d-flex flex-row-reverse">
         <nav aria-label="Page navigation example">
-            @if ($pagos!=null)
-                {{ $pagos->links('appCliente.paginate') }}    
+            @if ($pagos != null)
+                {{ $pagos->links('appCliente.paginate') }}
             @endif
         </nav>
     </div>
